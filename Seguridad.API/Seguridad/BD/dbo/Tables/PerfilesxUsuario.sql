c8ffee8a1b06d4e514805c725faa841982793078
Funnyd <dquesadaa2004@gmail.com>
2026-03-24 12:07:21 -0600

    Fix Pequeño

--- Seguridad.API/Seguridad/BD/dbo/Tables/PerfilesxUsuario.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[PerfilesxUsuario] (
-    [IdUsuario] UNIQUEIDENTIFIER NOT NULL,
-    [IdPerfil]  INT              NOT NULL,
-    CONSTRAINT [FK_PerfilesxUsuario_Perfiles] FOREIGN KEY ([IdPerfil]) REFERENCES [dbo].[Perfiles] ([Id]),
-    CONSTRAINT [FK_PerfilesxUsuario_Usuarios] FOREIGN KEY ([IdUsuario]) REFERENCES [dbo].[Usuarios] ([Id])
-);
-
--- Seguridad/AutorizacionMiddleware/AutorizacionAbstracciones/Modelos/Perfil.cs
+++ Seguridad/AutorizacionMiddleware/AutorizacionAbstracciones/Modelos/Perfil.cs
@@ -8,7 +8,7 @@ namespace AutorizacionAbstracciones.Modelos
 {
     public class Perfil
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Nombre { get; set; }
     }
 }
