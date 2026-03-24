47c881ba4fb676d2b4e65830bb1cd661fb4e4b68
Funnyd <dquesadaa2004@gmail.com>
2026-03-24 12:14:21 -0600

    Prueba

--- Seguridad/AutorizacionMiddleware/AutorizacionAbstracciones/Entidades/Perfil.cs
+++ Seguridad/AutorizacionMiddleware/AutorizacionAbstracciones/Entidades/Perfil.cs
@@ -8,7 +8,7 @@ namespace AutorizacionAbstracciones.Entidades
 {
     public class Perfil
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Nombre { get; set; }
     }
 }
