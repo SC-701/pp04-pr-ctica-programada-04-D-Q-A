99e6ca099f8395a3434c8735e1684956cb0aae65
Funnyd <dquesadaa2004@gmail.com>
2026-03-26 11:37:06 -0600

    .

---
+++ Seguridad.API/Seguridad/BD/dbo/Stored Procedures/ObtenerPerfilesxUsuario.sql
@@ -0,0 +1,18 @@
+﻿
+CREATE PROCEDURE ObtenerPerfilesxUsuario 
+	-- Add the parameters for the stored procedure here
+	@NombreUsuario VARCHAR(MAX),
+	@CorreoElectronico VARCHAR(MAX)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT Perfiles.Id, Perfiles.Nombre
+	FROM   Perfiles INNER JOIN
+					PerfilesxUsuario ON Perfiles.Id = PerfilesxUsuario.IdPerfil INNER JOIN
+					Usuarios ON PerfilesxUsuario.IdUsuario = Usuarios.Id
+	WHERE  (Usuarios.NombreUsuario = @NombreUsuario) OR (Usuarios.CorreoElectronico = @CorreoElectronico)
+END
